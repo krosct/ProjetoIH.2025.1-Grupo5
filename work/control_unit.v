219cbc9d787f2a9fce82100e33cc0a1e67b8703d
Vinícius Vieira Moreira <vvm@cin.ufpe.br>
2025-06-28 22:21:02 -0300

    Add Opcodes and functs

--- work/control_unit.v
+++ work/control_unit.v
@@ -24,7 +24,6 @@ module control_unit (
     output reg ControlB,
     output reg ALUOut,
     output reg DivMult,
-    output reg ALUOut,
     output reg Hi,
     output reg Lo,
     // Mux
@@ -37,22 +36,53 @@ module control_unit (
     output reg[2:0] MemToReg,
     output reg [1:0] ShiftFuncSrc,
     output reg [1:0] SelectByteSrc,
-    output reg SelectByte
+    output reg SelectByte,
     // Controller for reset
-    output reg reset_out
+    output reg rst_out
 
 );
 // Variables
 reg [2:0] state;
 reg [2:0] COUNTER;
 
-// Parameters (Constants)
+// Machine States
+parameter ST_READ_F_MEMORY = 5'd0;
 
+// Parameters (Constants)
 // Opcodes
+parameter INST_R = 6'h0x0;
+  // Functs
+parameter ADD = 6'h0x20;
+parameter AND = 6'h0x24;
+parameter DIV = 6'h0x1a;
+parameter MULT = 6'h0x18;
+parameter JR = 6'h0x8;
+parameter MFHI = 6'h0x10;
+parameter MFLO = 6'h0x12;
+parameter SLL = 6'h0x0;
+parameter SLT = 6'h0x2a;
+parameter SRA = 6'h0x3;
+parameter SUB = 6'h0x22;
+parameter XCHG = 6'h0x5;
+// Tipo I
+parameter ADDI = 6'h0x8;
+parameter BEQ = 6'h0x4;
+parameter BNE = 6'h0x5;
+parameter SLLM = 6'h0x1;
+parameter LB = 6'h0x20;
+parameter LUI = 6'h0xf;
+parameter LW = 6'h0x23;
+parameter SB = 6'h0x28;
+parameter SW = 6'h0x2b;
+// TIPO J
+parameter J = 6'h0x2;
+parameter JAL = 6'h0x3;
+initial begin
+    //
+end
 
 always @(posedge clk) begin
-
-
+    //
 end
 
 endmodule
